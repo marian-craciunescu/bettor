159e4beab15d20f4f1eb0e167f44fd25a12a649a
Marian Craciunescu <m.craciunescu@tarent.de>
2016-06-06 23:07:27 +0300

    Added initial demo DAO for users.Setting up of the projects

---
+++ src/main/java/com/bettor/BettorApplication.java
@@ -0,0 +1,12 @@
+package com.bettor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettorApplication.class, args);
+	}
+}
